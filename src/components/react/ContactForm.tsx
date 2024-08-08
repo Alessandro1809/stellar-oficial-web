816600f23e46e8a3b7f48ab6a461447f1b649dc2
Alessandro1809 <dalessandro18dg@gmail.com>
2024-08-07 18:19:24 -0600

    agregando lik y texto informativo

--- src/components/react/ContactForm.tsx
+++ src/components/react/ContactForm.tsx
@@ -4,6 +4,7 @@ import { type Email } from "@/lib/types/mailTypes";
 import { sendEmail } from "@/lib/emailservice"
 export const ContactForm = () => {
 
+  const [alert, setAlert]=useState('');
   const mail: Email = {
     name: "",
     email: "",
@@ -15,18 +16,29 @@ export const ContactForm = () => {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
+      if (usermail.name==='' || usermail.email==='' || usermail.message==='') {
+        setAlert('Please, fill all the fields!')
+        setTimeout(() => {
+          setAlert('');
+        }, 3000);
+        return;
+      }
       await sendEmail(
         usermail.name,
         usermail.email,
         usermail.message
       );
-      setUsermail(mail); 
+      setAlert('Yeah!! Email sent with success!');
+      setUsermail(mail);
+      setTimeout(() => {
+        setAlert('');
+      }, 3000);
     } catch (error) {
       console.log(error);
     }
   }
   return (
-    <>
+    <> {alert && <p className="absolute left-1/2 top-0 transform -translate-x-1/2 -translate-y-1/2 text-lg font-medium text-center text-green-200  animate-blurred-fade-in transition-all duration-1000 ">{alert}</p>}
       <form onSubmit={(e) => handleSubmit(e)} className="flex flex-col rounded-lg p-4 gap-6 transition-all duration-1000">
           <div className="w-full flex flex-col">
             <label className="text-xl font-medium" htmlFor="name">Name:</label>
@@ -64,8 +76,10 @@ export const ContactForm = () => {
           </div>
           
           <input className=" hover:cursor-pointer hover:from-white/40 hover:via-cyan-300/40 hover:to-indigo-500/40 w-32 my-4 bg-gradient-to-tr from-white/20 via-cyan-300/20 to-indigo-500/20  font-medium rounded-md p-2 transition-all duration-1000 text-wrap whitespace-normal" type="submit" value="Send" />
+
+         
       </form>
-    
+
     </>
   )
 }
